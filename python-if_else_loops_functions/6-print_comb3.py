0d336cd846c7aa7c4699c8cac075dd1c92284a53
Arlind3553 <georgetesco@yahoo.com>
2023-09-26 01:38:55 -0700

    task 1

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range (i + 1, 10):
-        print("{.02d}".format(i, j), end = ", ")
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end = ", ")
