df11e3c91a9028bb26564fcc1c7d8c12d0df0ae1
Arlind3553 <georgetesco@yahoo.com>
2023-09-27 03:27:02 -0700

    task 1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif l == 1:
         print("1 argument:")
     else:
-        print("{}: arguments:".format(l))
+        print("{} arguments:".format(l))
     for i in range (l):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
