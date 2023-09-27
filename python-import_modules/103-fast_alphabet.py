2b8f36cbe766d068b2f0fefdea7b736c00383ee5
Arlind3553 <georgetesco@yahoo.com>
2023-09-27 05:01:10 -0700

    task 1

--- python-import_modules/103-fast_alphabet.py
+++ python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-from itertools import accumulate, starmap, repeat
-print(''.join(map(lambda x: chr(x), range(65, 91))), end='\n')
+print(''.join(map(lambda x: chr(x), range(65, 91))))
+
