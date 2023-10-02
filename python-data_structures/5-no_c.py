d63683885b0a4d99fd9eb1c3b0afcece7fb41a35
Arlind3553 <georgetesco@yahoo.com>
2023-10-02 05:57:08 -0700

    task 1

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,7 +3,6 @@ def no_c(my_string):
     new_str = ""
     for ch in my_string:
         if ch != c and ch != C:
-            new_str = ch
-
+            new_str = new_str + ch
     return new_str
 
