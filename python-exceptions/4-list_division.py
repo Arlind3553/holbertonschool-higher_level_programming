e54a1fc78c35acc244d29e110b3a498d004927cd
Arlind3553 <georgetesco@yahoo.com>
2023-10-05 02:58:03 -0700

    TASK 4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+def list_division(my_list_1, my_list_2, list_length):
+    nList = []
+    for i in range(list_length):
+        try:
+            result = my_list_1 / my_list_2
+        except TypeError:
+            print("wrong type")
+            resut = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            resut = 0
+        except IndexError:
+            print("out of range")
+            resut = 0
+        finally:
+            nList.append(result)
+    return nList
