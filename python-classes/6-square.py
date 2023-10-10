ba42516c486dcd63c0913f013a5ddc0c29a484a7
Arlind3553 <georgetesco@yahoo.com>
2023-10-10 02:56:01 -0700

    6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -10,24 +10,54 @@ class Square:
     This is the documentation for the Square class.
     The class represents a geometric square and associated operations.
     """
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """
         python3 -c 'print(__import__("my_module").my_function.__doc__)'
         python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
         """
         self.__size = size
+        self.position = position
 
     @property
     def size(self):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    @property
+    def position(self):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(i, int) for i in value) or
+                not all(i >= 0 for i in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """
@@ -36,26 +66,19 @@ class Square:
         """
         return self.__size ** 2
 
-    @property
-    def position(self):
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        if (isinstance(value, tuple) is False or len(value) != 2
-                or (isinstance(value[0], int) is False or value[0] < 0)
-                or (isinstance(value[1], int) is False or value[1] < 0)):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
-
     def my_print(self):
-        if size == 0:
-            print('')
-        for i in range(self.__position[1]):
-            print('')
-        for row in (self.__size):
-            for j in range(self.__position[0]):
-                print(" ", end="")
-            for col in range(self.__size):
-                print("#", end='')
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        if self.__size == 0:
             print()
+        else:
+            for ver_space in range(self.__position[1]):
+                print()
+            for row in range(self.__size):
+                for hor_space in range(self.__position[0]):
+                    print(' ', end='')
+                for col in range(self.__size):
+                    print('#', end='')
+                print()
