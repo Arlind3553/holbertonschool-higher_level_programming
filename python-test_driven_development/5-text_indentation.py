23dc28b7e4f57b02576f596d6806beedb51c8d38
Arlind3553 <georgetesco@yahoo.com>
2023-10-12 04:26:29 -0700

    task 1

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""
-python3 -c 'print(__import__("my_module").__doc__)'
-"""
+"""Text indentation module"""
 
 
-def say_my_name(first_name, last_name=""):
-    """
-    python3 -c 'print(__import__("my_module").my_function.__doc__)'
-    """
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+def text_indentation(text):
+    """Indents text whenever needed"""
+    if type(text) is not str:
+        raise TypeError("text must be string")
+    char = ['.', '?', ':']
+    i = 0
+    while i in range(len(text)):
+        print('{}'.format[textp[i], end="")
+        if text[[i] in char:
+            print('\n')
+            while text[i + 1] == " ":
+                i += 1
+        i += 1
