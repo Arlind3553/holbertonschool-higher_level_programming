011d122393b65f0516c3db529d9a4e2c1d942b80
Arlind3553 <georgetesco@yahoo.com>
2023-10-13 01:47:40 -0700

    task 1

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -45,6 +45,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * self.width + 2 * self.height
+
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ("")
