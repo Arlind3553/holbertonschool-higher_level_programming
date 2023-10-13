300e7d0abc8c88edc25886597a6fa46c1cf7b4af
Arlind3553 <georgetesco@yahoo.com>
2023-10-13 02:49:11 -0700

    task 1

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -56,3 +56,5 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+        def __repr__(self):
+            return f"Rectangle({self.__width}, {self.__height})"
