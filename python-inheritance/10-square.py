7cdfac110564b965427c7a280f90d443eb01a35a
Arlind3553 <georgetesco@yahoo.com>
2023-10-17 05:06:21 -0700

    task 1

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -5,6 +5,7 @@
 Rectangle = __import__('9-rectangle').Rectangle
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Square(Rectangle):
     '''Subclass of Rectangle named square'''
     def __init__(self, size):
