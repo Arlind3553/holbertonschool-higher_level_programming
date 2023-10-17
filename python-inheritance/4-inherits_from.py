48a8b41d32929a948aac09b3c3111e5e0ed40715
Arlind3553 <georgetesco@yahoo.com>
2023-10-17 04:19:49 -0700

    task 1

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,8 +6,4 @@ python3 -c 'print(__import__("my_module").__doc__)'
 
 def inherits_from(obj, a_class):
     '''Finds if an object is an instance of a class'''
-    if isinstance(obj, a_class):
-        if obj.__class is a_class:
-            return False
-        return True
-    return True
+    return isinstance(obj, a_class) and type(obj) is not a_class
