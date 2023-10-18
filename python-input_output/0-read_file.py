7f121c76a39517233dac11f8c85829d8eb63dcf1
Arlind3553 <georgetesco@yahoo.com>
2023-10-18 01:55:41 -0700

    taks 1

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def read_file(filename=""):
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            content = file.read()
+            print(content, end='')
