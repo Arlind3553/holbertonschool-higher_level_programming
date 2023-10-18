e1888937ebf145116435e5a23ec15ee9ca484dab
Arlind3553 <georgetesco@yahoo.com>
2023-10-18 01:56:35 -0700

    task 1

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,3 +6,5 @@ def read_file(filename=""):
         with open(filename, 'r', encoding='utf-8') as file:
             content = file.read()
             print(content, end='')
+    except FileNotFoundError:
+        pass
