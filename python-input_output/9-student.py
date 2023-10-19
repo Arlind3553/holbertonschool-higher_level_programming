7521a2736fd02d80dccb4009fc76e75bef47af03
Arlind3553 <georgetesco@yahoo.com>
2023-10-19 04:03:26 -0700

    task 1

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -8,7 +8,7 @@ class Student:
     """
     python3 -c 'print(__import__("my_module").MyClass.__doc__)'
     """
-    def __innit__(self, first_name, last_name, age):
+    def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
