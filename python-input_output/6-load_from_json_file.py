d371031841f6b77c2dca2d9369af2b64cb9e65e7
Arlind3553 <georgetesco@yahoo.com>
2023-10-19 03:48:06 -0700

    task 1

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -4,14 +4,9 @@ python3 -c 'print(__import__("my_module").__doc__)'
 """
 
 
-import json
-
-
-def load_from_json_file(filename):
+def class_to_json(obj):
     """
     'print(__import__("my_module").my_function.__doc__)'
     python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
     """
-    with open(filename, 'r', encoding='utf-8') as file:
-        read = file.read()
-    return json.loads(read)
+    return obj.__dict__
