0ccefa17715131326701e8776bc8e4de23bdc798
Arlind3553 <georgetesco@yahoo.com>
2023-10-23 02:50:31 -0700

    task 1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -9,11 +9,11 @@ class Base:
     Class for Base objects
     '''
 
-    __nd_objects = 0
+    __nb_objects = 0
 
     def __init__(self, id=None):
         if id is not None:
-            self.__id = id
+            self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = Base.__nd_objects
+            self.id = Base.__nb_objects
