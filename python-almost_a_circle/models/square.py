8941d6f040b5edb44ee8bfd89ce57548511fe030
Arlind3553 <georgetesco@yahoo.com>
2023-10-24 02:26:33 -0700

    task 1

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+Module for demostrattive purposes
+'''
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''
+    Class for Square objects
+    '''
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.__width = value
+        self.__height = value
+
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
