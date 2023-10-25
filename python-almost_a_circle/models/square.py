c4148013be39f6f1ddee57715489f059a87c43be
Arlind3553 <georgetesco@yahoo.com>
2023-10-25 01:59:17 -0700

    task 14

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -47,5 +47,5 @@ class Square(Rectangle):
                 "id" = self.id,
                 "size" = self.size,
                 "x" = self.x,
-                "y" = self.y,
+                "y" = self.y
                 }
