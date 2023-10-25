4d9be74fb100f77e8285fbfdca432634d732a502
Arlind3553 <georgetesco@yahoo.com>
2023-10-25 02:06:54 -0700

    task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,3 +17,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        '''
+        Return list of dictionaries to JSON string
+        '''
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
