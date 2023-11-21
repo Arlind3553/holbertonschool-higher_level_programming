a8570ad094d7b83ee349f5b6d2e7b5ca7772d174
Arlind3553 <georgetesco@yahoo.com>
2023-11-21 12:58:12 -0800

    task 1

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,6 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-SELECT AVG(score) AS AverageScore
-FROM hbtn_0c_0.second_table;
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
