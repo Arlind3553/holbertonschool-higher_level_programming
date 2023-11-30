a8ee1c36c16db02db3d93c5a198b77f230ad4748
Arlind3553 <georgetesco@yahoo.com>
2023-11-30 03:38:00 -0800

    task 1

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+List all states from the database hbtn_0e_0_usa
+"""
+
+
+from sqlalchemy.orm import Session
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+
+if __name__ == '__main__':
+    db = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3])
+    eng = create_engine(db)
+    Base.metadata.create_all(eng)
+    session = Session(eng)
+    new_session = State(name="Louisiana")
+    state = session.query(State).filter_by(id=2).first()
+    if state:
+        state.name = "New Mexico"
+        sesion.commit()
+    session.close()
